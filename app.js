2c817fcc38235855a47f0afac28adc57c2acf401
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-11-25 22:02:53 -0500

    generate the HTML file

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-var message = 'Hello Node!';
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-var sum = 5 + 3;
-
-console.log(message);
-console.log(sum);
-
-
-
-const profileDataArgs=process.argv.slice(2,process.argv.length);
-
-
-const printProfileData = profileDataArr => {
-for(let i=0;i<profileDataArr.length; i += 1) {
-console.log(profileDataArr[i]);
-}
-console.log('================');
-profileDataArr.forEach((profileItem) => {
-console.log(profileItem)
-});
-};
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
